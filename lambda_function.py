635f0eb6594ac1ece977de78dad17ae0c82cf031
keno-tech <techin.keno2@gmail.com>
2024-06-19 20:16:00 +0700

    added s3 functionality

--- lambda_function.py
+++ lambda_function.py
@@ -15,29 +15,33 @@ s3_bucket_name = os.getenv('S3_BUCKET_NAME')
 
 def lambda_handler(event, context):
     try:
-        # Parse the incoming LINE message event
+        print("Received event:", json.dumps(msg, indent=2))
+
         msg = json.loads(event['body'])
-        print("Received message:", json.dumps(msg, indent=2))
-        # Process each event in the message
-        for event in msg['events']:
+
+        for event in msg.get('events', []):
             # Ensure the event is a message event
-            if event['type'] == 'message' and event['message']['type'] == 'text':
-                # Extract the reply token and user message text
-                reply_token = event['replyToken']
-                user_message = event['message']['text']
-                
-                # Reply to the user with the same text they sent
-                line_bot_api.reply_message(
-                    reply_token,
-                    TextSendMessage(text=user_message)
-                )
-        # Return a 200 response with the original message for logging/debugging purposes
+            if event['type'] == 'message':
+                event_type = event['message']['type']
+                s3 = boto3.client('s3')
+                message_content = line_bot_api.get_message_content(event['message']['id'])
+                print("Message Content:", json.dumps(msg, indent=2)) 
+                data = message_content.content
+
+                if event_type == 'image':
+                    object_key = f'{event["message"]["id"]}.jpg'
+                    s3.put_object(Bucket=s3_bucket_name, Key=object_key, Body=data)
+                    print(f"Image uploaded to S3: {s3_bucket_name}/{object_key}")
+
+                elif event_type == 'video':
+                    pass
+
+
         return {
             "statusCode": 200,
             "body": json.dumps({"message": "Messages processed successfully"})
         }
     except Exception as e:
-        # Log the exception and return a 500 Internal Server Error response
         return {
             "statusCode": 500,
             "body": json.dumps({"message": f"{e}"})
