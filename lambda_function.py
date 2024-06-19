af0faaca854d1d7fdd0c86c9d64103d7b0c141ea
keno-tech <techin.keno2@gmail.com>
2024-06-19 20:26:47 +0700

    testing bucket

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
 import json
 import boto3
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import TextSendMessage
 import os
 from dotenv import load_dotenv
 
@@ -10,32 +9,34 @@ load_dotenv()
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
+s3 = boto3.client('s3')
 s3_bucket_name = os.getenv('S3_BUCKET_NAME')
-
 
 def lambda_handler(event, context):
     try:
-
+        # Parse the incoming LINE message event
         msg = json.loads(event['body'])
         print("Received event:", json.dumps(msg, indent=2))
 
         for event in msg.get('events', []):
             # Ensure the event is a message event
             if event['type'] == 'message':
-                event_type = event['message']['type']
-                s3 = boto3.client('s3')
-                message_content = line_bot_api.get_message_content(event['message']['id'])
-                print("Message Content:", json.dumps(msg, indent=2)) 
-                data = message_content.content
+                message_type = event['message']['type']
+                message_id = event['message']['id']
+                
+                # Get message content from LINE
+                message_content = line_bot_api.get_message_content(message_id)
 
-                if event_type == 'image':
-                    object_key = f'{event["message"]["id"]}.jpg'
+                # Determine the type of message and handle accordingly
+                if message_type == 'image':
+                    object_key = f'{message_id}.jpg'
+                    data = message_content.content
                     s3.put_object(Bucket=s3_bucket_name, Key=object_key, Body=data)
-                    print(f"Image uploaded to S3: {s3_bucket_name}/{object_key}")
-
-                elif event_type == 'video':
+                    print(f"Image uploaded to S3: s3://{s3_bucket_name}/{object_key}")
+                    
+                elif message_type == 'video':
+                    # Handle video messages if needed
                     pass
-
 
         return {
             "statusCode": 200,
@@ -44,5 +45,5 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             "statusCode": 500,
-            "body": json.dumps({"message": f"{e}"})
+            "body": json.dumps({"message": str(e)})
         }
